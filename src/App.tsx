4f2a791cf8a5245a6f660f2f54a987a95202fd9a
Shoghy Martinez <shoghymartinez@gmail.com>
2023-05-20 09:36:49 -0400

    Preparativos listos

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,15 @@
-import { useState } from "react"
-import "./styles.css"
+import {Route, Routes, NavLink} from "react-router-dom";
 
-
-interface ToDoListElement{
-  id: string,
-  title: string,
-  completed: boolean
-}
 
 export default function App(){
-  const [newItem, setNewItem] = useState<string>("");
-  const [todos, setTodos] = useState<ToDoListElement[]>([]);
-
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void{
-    e.preventDefault();
-    if(newItem === "") return;
-
-    setTodos((currentTodos) => {
-      return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false}]
-    })
-    setNewItem("");
-  }
-
-  function deleteTodo(id: string): void{
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id);
-    });
-  }
-
-  function toggleTodo(id:string, completed:boolean){
-    setTodos((currentTodos) => {
-      return currentTodos.map(todo => {
-        if(todo.id === id){
-          return {...todo, completed};
-        }
-        return todo;
-      });
-    });
-  }
-
-  return <>
-  <form className="new-item-form" onSubmit={handleSubmit}>
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input
-      type="text"
-      id="item"
-      value={newItem}
-      onChange={e => setNewItem(e.target.value)}
-      />
-    </div>
-    <button className="btn">Add</button>
-  </form>
-  <h1>Todo List</h1>
-  <ul className="list">
-    {(() => {
-      let listItem = [];
-      if(todos.length === 0){
-        return <h3>There are no todos</h3>
-      }
-      for(let i = 0; i < todos.length; ++i){
-        let todo = todos[i];
-        listItem.push(<li key={todo.id}>
-          <label>
-            <input type="checkbox" checked={todo.completed} onChange={e =>toggleTodo(todo.id, e.target.checked)}/>
-            {todo.title}
-          </label>
-          <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button>
-        </li>);
-      }
-      return listItem;
-    })()}
-  </ul>
-  </>
+  return <Routes>
+    <Route
+    path="/"
+    element={<h1><NavLink to="/adios-mundo">Hola mundo</NavLink></h1>}
+    />
+    <Route
+    path="/adios-mundo"
+    element={<h1><NavLink to="/">Adiós mundo</NavLink></h1>}
+    />
+  </Routes>
 }
