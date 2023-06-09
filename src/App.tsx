4c09da45f717dacf6a84d41055421c6e37a0cbaf
Shoghy Martinez <shoghymartinez@gmail.com>
2023-06-09 09:25:46 -0400

    Adding formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-import {Route, Routes, Link} from "react-router-dom";
-import FormComponent from "./components/FormComponent";
+
 
 export default function App(){
-  return <Routes>
-    <Route
-    path="/"
-    element={<h1><Link to="/adios-mundo">Hola mundo</Link></h1>}
-    />
-    <Route
-    path="/adios-mundo"
-    element={<h1><Link to="/">Adiós mundo</Link></h1>}
-    />
-    <Route
-    path="/prueba"
-    element={<FormComponent>
-      <h1></h1>
-      <h2></h2>
-      <p></p>
-    </FormComponent>}
-    />
-  </Routes>
+  return <></>
 }
