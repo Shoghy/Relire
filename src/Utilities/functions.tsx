e9aae9315e4ed74d9196b1a159689a7c266860c9
Shoghy Martinez <shoghymartinez@gmail.com>
2023-07-08 11:05:27 -0400

    RandomInt Function

--- src/Utilities/functions.tsx
+++ src/Utilities/functions.tsx
@@ -30,13 +30,19 @@ export function GetEnumValues(val: string): string[] {
 export function RandomString(length: number) {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
+  const charactersLength = characters.length - 1;
   let counter = 0;
   while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(RandomInt(0, charactersLength));
     counter += 1;
   }
   return result;
+}
+
+export function RandomInt(minInclusive:number, maxInclusive:number){
+  return Math.floor(
+    Math.random() * (maxInclusive - minInclusive + 1)
+  )  + minInclusive;
 }
 
 interface IColumnToInput{
