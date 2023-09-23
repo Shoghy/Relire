41f8e15a012af4fc702906323a4d36d229a7564e
Shoghy Martinez <shoghymartinez@gmail.com>
2023-09-23 11:00:23 -0400

    Vuelta al principio

---
+++ src/utilities/types.ts
@@ -0,0 +1,53 @@
+export interface Dictionary<T> {
+  [Key: string]: T;
+}
+
+export interface IDataBase{
+  author:string,
+  dbName: string,
+  tables?: Dictionary<Dictionary<IColumn>>,
+  tablesData?: Dictionary<TableRow>
+}
+
+export type ColumnType = "string" | "int" | "float" | "bool" | "date" | "datetime" | "enum";
+
+export type ColumnValue = string | number | boolean;
+
+export interface IApiRequest extends Dictionary<any>{
+  auth: string,
+  type: "user" | "key"
+}
+
+export interface DefaultReturnedError extends Error{
+  code: string
+}
+
+export interface IApiResponse extends Dictionary<any>{
+  ok: boolean,
+  error?: DefaultReturnedError
+}
+
+export interface IColumn{
+  type: ColumnType,
+  notNull: boolean,
+  default?: ColumnValue,
+  unique: boolean,
+  foreingKey?: IForeingKey,
+  enum?: string[],
+  autoIncrement?: boolean
+}
+
+//                     UID        Column     Value
+export type TableRow = Dictionary<Dictionary<ColumnValue>>;
+
+export interface IForeingKey{
+  tableName: string,
+  column: string
+}
+
+export interface IErrorElement{
+  element: React.JSX.Element,
+  todoBien: boolean
+}
+
+export type AsyncFunc<T> = (... args: any) => Promise<T>;
