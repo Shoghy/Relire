1bd125b4671e378393001dbb17eed9eb47535d38
Shoghy Martinez <shoghymartinez@gmail.com>
2023-12-03 13:46:38 -0400

    🎨 Dando formato a LogIn code

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,4 +1,4 @@
-import {Formik, Form, Field, ErrorMessage, FormikHelpers} from "formik";
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
 import { useEffect, useState } from "react";
 import { auth } from "../../utilities/DBclient";
 import { AuthErrorCodes, AuthError } from "firebase/auth";
@@ -7,12 +7,12 @@ import { MainPage } from "../../utilities/PageLocations";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { AsyncAttempter } from "../../utilities/functions";
 
-interface ILogIn{
+interface ILogIn {
   email?: string,
   password?: string
 }
 
-export default function LogInForm(){
+export default function LogInForm() {
   const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   const navigate = useNavigate();
   const [sendedForm, changeSendedForm] = useState<boolean>(false);
@@ -20,35 +20,35 @@ export default function LogInForm(){
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      if(user !== undefined && user !== null){
+      if (user !== undefined && user !== null) {
         navigate(MainPage);
       }
     });
   }, []);
 
-  function validate(values:ILogIn){
+  function validate(values: ILogIn) {
     const errors: ILogIn = {};
 
     //Validar email
-    if(values.email === undefined || !values.email.match(validEmailRegex)){
+    if (values.email === undefined || !values.email.match(validEmailRegex)) {
       errors.email = "Add a valid email";
     }
-    
+
     //Validar contraseña
-    if(values.password === undefined){
+    if (values.password === undefined) {
       errors.password = "Debes añadir una contraseña";
-    }else if(values.password.length < 6){
+    } else if (values.password.length < 6) {
       errors.password = "Passwords are at least 6 characters long";
     }
 
     return errors;
   }
 
-  async function onSubmit(values:ILogIn, helpers : FormikHelpers<ILogIn>){
+  async function onSubmit(values: ILogIn, helpers: FormikHelpers<ILogIn>) {
     changeSendedForm(true);
     changeAuthError(undefined);
 
-    if(values.email === undefined || values.password === undefined){
+    if (values.email === undefined || values.password === undefined) {
       changeSendedForm(false);
       return;
     }
@@ -57,24 +57,24 @@ export default function LogInForm(){
     const logIn = signInWithEmailAndPassword(auth, values.email, values.password);
     const [, logInError] = await AsyncAttempter<AuthError>(() => logIn);
 
-    if(!logInError){
+    if (!logInError) {
       changeSendedForm(false);
       return;
     }
 
     const errorCode = logInError.code;
 
-    switch(errorCode){
-      case AuthErrorCodes.USER_DELETED:{
+    switch (errorCode) {
+      case AuthErrorCodes.USER_DELETED: {
         changeAuthError("User don't exist");
         helpers.resetForm();
         break;
-      }case AuthErrorCodes.INVALID_PASSWORD:{
+      } case AuthErrorCodes.INVALID_PASSWORD: {
         changeAuthError("Constraseña incorrecta");
         helpers.setFieldValue("email", "", false);
         break;
       }
-      default:{
+      default: {
         changeAuthError("Something went wrong");
         break;
       }
@@ -83,15 +83,15 @@ export default function LogInForm(){
     changeSendedForm(false);
   }
 
-  const initialValues: ILogIn = {email: "", password: ""};
+  const initialValues: ILogIn = { email: "", password: "" };
   return <Formik initialValues={initialValues} onSubmit={onSubmit} validate={validate}>
-    {({errors}) => (
+    {({ errors }) => (
       <Form>
         {authError && <p>{authError}</p>}
-        <Field type="email" name="email" disabled={sendedForm}/><br/>
-        <ErrorMessage name="email" component={() => <p>{errors.email}</p>}/>
-        <Field type="password" name="password" disabled={sendedForm}/><br/>
-        <ErrorMessage name="password" component={() => <p>{errors.password}</p>}/>
+        <Field type="email" name="email" disabled={sendedForm} /><br />
+        <ErrorMessage name="email" component={() => <p>{errors.email}</p>} />
+        <Field type="password" name="password" disabled={sendedForm} /><br />
+        <ErrorMessage name="password" component={() => <p>{errors.password}</p>} />
         <button type="submit" className="btn">Submit</button>
       </Form>
     )}
