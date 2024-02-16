d44c8c5da847b766f81c1ffe7b28faa904b0cdcc
Shoghy Martinez <shoghymartinez@gmail.com>
2024-02-16 09:01:06 -0400

    💄NavBar styles added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,39 @@
-import { auth } from "../utilities/DBclient";
+import { auth } from "@/utilities/DBclient";
+import { useEffect, useState } from "react";
 
-export default function NavBar(){
-  function btnLogOut(){
+export default function NavBar() {
+  const [showLogOut, setShowLogOut] = useState(false);
+  function LogOut(){
     auth.signOut();
+  }
+  async function CheckUserStatus(){
+    await auth.authStateReady();
+    if(auth.currentUser == null) return;
+    setShowLogOut(true);
+  }
+
+  useEffect(() => {
+    CheckUserStatus();
+  }, []);
+
+  function LogOutComponent() {
+    if(!showLogOut) return;
+
+    return (
+      <div className="log-out">
+        <div onClick={() => LogOut()} style={{
+          display: "inline-flex",
+          cursor: "pointer"
+        }}>
+
+          <div className="peach-circle" style={{
+            height: 18,
+            marginRight: 3
+          }}></div>
+          <span>LogOut</span>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -10,8 +41,10 @@ export default function NavBar(){
       className="nav-bar"
     >
       <div className="logo-container">
-        
+        <span>RELIRE</span>
+        <img src="/svgs/Flechas.svg" height={70} />
       </div>
+      <LogOutComponent/>
     </nav>
   );
 }
