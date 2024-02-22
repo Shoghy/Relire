3dc36f5749493df51643e552e32d553b259bd9d2
Shoghy Martinez <shoghymartinez@gmail.com>
2024-02-22 14:16:12 -0400

    💄✨Using custom alerts en mainpage

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,57 +2,72 @@ import NavBar from "@/components/NavBar";
 import styles from "./main.module.css";
 import { ChangeBodyColor, RemoveIndexOfArray } from "@/utilities/functions";
 import DBButton from "./db_button";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CreateDatabase, DeleteDatabase, GetDatabases, auth } from "@/utilities/DBclient";
 import { DB, LogIn } from "@/utilities/PageLocations";
 import { useNavigate } from "react-router-dom";
-import { BasicDBInfo, IApiResponse } from "@/utilities/types";
+import { BasicDBInfo } from "@/utilities/types";
+import { selfCustomAlert, selfDAlert } from "@/components/custom_alert";
+import { selfLoadingCurtain } from "@/components/loading_curtain";
+import TextInput from "@/components/TextInput";
 
+interface CustomAlertInfo {
+  title: string
+  message: string
+  inputValue: string
+  submitFunction: () => any
+  dbUID: string
+  dbName: string
+}
+
+const customAlert = selfCustomAlert();
+const DAlert = selfDAlert();
+const loadingScreen = selfLoadingCurtain(true);
 export default function Main() {
   const navigate = useNavigate();
   ChangeBodyColor("var(--dark-green)");
-  const [userDBs, setUserDBs] = useState<BasicDBInfo[] | string>();
+  const [userDBs, setUserDBs] = useState<BasicDBInfo[]>([]);
+  const [alertInfo, setAlertInfo] = useState<CustomAlertInfo>({
+    inputValue: "",
+    message: "",
+    submitFunction: () => { return; },
+    title: "",
+    dbName: "",
+    dbUID: ""
+  });
+  const alertInfoRef = useRef(alertInfo);
+  alertInfoRef.current = alertInfo;
 
   useEffect(() => {
+    loadingScreen.open();
     LoadDatabases();
   }, []);
 
-  async function LoadDatabases(){
+  async function LoadDatabases() {
     await auth.authStateReady();
-    if(auth.currentUser === null){
+  
+    if (auth.currentUser === null) {
       navigate(LogIn);
       return;
     }
-
+    
     const dbList = await GetDatabases();
+    loadingScreen.close();
 
-    if(!dbList.ok){
-      setUserDBs("An error occured.");
+    if (!dbList.ok) {
+      DAlert.openWith({
+        title: "Error",
+        message: dbList.error?.message
+      });
+      return;
     }
 
-    dbList.dbInfos ??= [];
-    setUserDBs(dbList.dbInfos);
+    setUserDBs(dbList.dbInfos ?? []);
   }
 
-  function EnumarateDatabases(){
-    if(userDBs === undefined){
-      return (
-        <Message>
-          Loading databases...
-        </Message>
-      );
-    }
-
-    if(typeof userDBs === "string"){
-      return (
-        <Message>
-          {userDBs}
-        </Message>
-      );
-    }
-
+  function EnumarateDatabases() {
     const buttons: React.JSX.Element[] = [];
-    for(let i = 0; i < userDBs.length; ++i){
+    for (let i = 0; i < userDBs.length; ++i) {
       buttons.push(
         <DBButton
           key={i}
@@ -60,7 +75,7 @@ export default function Main() {
           onClick={() => navigate(DB(userDBs[i].dbUID as string))}
           onXBtnClick={(e) => {
             e.stopPropagation();
-            RemoveDatabase(userDBs[i].dbUID, userDBs[i].dbName);
+            OnDBXClick(userDBs[i].dbUID, userDBs[i].dbName);
           }}
         >
           <h3>{userDBs[i].dbName}</h3>
@@ -72,68 +87,96 @@ export default function Main() {
   }
 
   async function CreateDB() {
-    const dbName = prompt("Insert the name of the database");
-    if (!dbName) return;
-    const cdbAsync = CreateDatabase(
-      dbName
-    );
+    const dbName = alertInfoRef.current.inputValue;
+    loadingScreen.open();
+    customAlert.close();
 
-    let newDB: IApiResponse;
-    try {
-      newDB = await cdbAsync;
-    } catch (error) {
+    if (!dbName) {
+      DAlert.openWith({
+        title: "You need to add a name to your database"
+      });
+      loadingScreen.close();
       return;
     }
 
+    const newDB = await CreateDatabase(
+      dbName
+    );
+    loadingScreen.close();
+
     if (!newDB.ok) {
-      switch (newDB.error?.code) {
-        case "db-limit": {
-          alert(newDB.error.message);
-          break;
-        }
-        default: {
-          alert("We were not able to process yor request");
-          break;
-        }
-      }
+      DAlert.openWith({
+        title: "Error",
+        message: newDB.error?.message
+      });
       return;
     }
 
     setUserDBs((current) => {
-      if(!Array.isArray(current)){
-        current = [];
-      }
       current.push({ dbName, dbUID: newDB.dbUID as string });
       return [...current];
     });
   }
 
-  async function RemoveDatabase(dbUID: string, dbName: string){
-    const confirmDBName = prompt(`Insert the name of the database: ${dbName}`);
+  async function DeleteDB() {
+    customAlert.close();
+    loadingScreen.open();
+    
+    const dbUID = alertInfoRef.current.dbUID;
+    const dbName = alertInfoRef.current.dbName;
+    const confirmDBName = alertInfoRef.current.inputValue;
+
     if (confirmDBName !== dbName) {
-      alert("The names don't match");
+      DAlert.openWith({
+        title: "The names don't match"
+      });
+      loadingScreen.close();
       return;
     }
     const response = await DeleteDatabase(dbUID, dbName);
+    loadingScreen.close();
 
-    if(!response.ok){
-      alert(response.error?.message);
+    if (!response.ok) {
+      DAlert.openWith({
+        title: "Error",
+        message: response.error?.message
+      });
       return;
     }
 
     setUserDBs((current) => {
-      if(!Array.isArray(current)){
-        return;
-      }
-
-      for(let i = 0; i < current.length; ++i){
+      for (let i = 0; i < current.length; ++i) {
         const dbInfo = current[i];
-        if(dbInfo.dbUID !== dbUID) continue;
+        if (dbInfo.dbUID !== dbUID) continue;
         current = RemoveIndexOfArray(current, i);
         break;
       }
 
-      return [... current];
+      return [...current];
+    });
+  }
+
+  function OnDBXClick(dbUID: string, dbName: string) {
+    customAlert.open();
+    setAlertInfo({
+      title: "Delete",
+      message: `Insert the name of the database: ${dbName}`,
+      inputValue: "",
+      dbName: dbName,
+      dbUID: dbUID,
+      submitFunction: DeleteDB
+    });
+  }
+
+  function OnAddBtnClick() {
+    customAlert.open();
+    setAlertInfo({
+      title: "Create Database",
+      message: "Insert the name of the new database",
+      inputValue: "",
+      dbName: "",
+      dbUID: "",
+      submitFunction: CreateDB
     });
   }
 
@@ -144,26 +187,35 @@ export default function Main() {
         <div className={styles.container}>
           <DBButton
             showXButton={false}
-            onClick={() => CreateDB()}
+            onClick={() => OnAddBtnClick()}
             containerClassName={styles["add-db-btn"]}
           >
             <i className="fa fa-plus" aria-hidden="true"></i>
           </DBButton>
-          <EnumarateDatabases/>
+          <EnumarateDatabases />
         </div>
       </div>
+      <loadingScreen.Element />
+      <DAlert.Element />
+      <customAlert.Element className={styles.alerts}>
+        <h1>{alertInfo.title}</h1>
+        <span>{alertInfo.message}</span>
+        <TextInput
+          type="text"
+          value={alertInfo.inputValue}
+          onChange={(e) => {
+            const value = e.currentTarget.value;
+            setAlertInfo((c) => {
+              c.inputValue = value;
+              return { ...c };
+            });
+          }}
+        />
+        <button
+          className={styles["submit-btn"]}
+          onClick={() => alertInfo.submitFunction()}
+        >Confirm</button>
+      </customAlert.Element>
     </>
-  );
-}
-
-function Message({children}:{children: string}){
-  return (
-    <h1 style={{
-      color: "var(--nyanza)",
-      width: "100%",
-      textAlign: "center"
-    }}>
-      {children}
-    </h1>
   );
 }
