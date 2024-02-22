a58474ca5848c2cb5780438858189babb068d722
Shoghy Martinez <shoghymartinez@gmail.com>
2024-02-22 10:22:34 -0400

    💄✨Loading curtain
    A div that put in itself in top on the screen with a loading animation in its center

---
+++ src/components/loading_curtain.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import styles from "./loading_curtain.module.css";
+
+export default function LoadingCurtain(){
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <div className={styles.loader}/>
+      </div>
+    </div>
+  );
+}
+
+export interface SelfLoadingCurtain{
+  Element: () => React.JSX.Element
+  isOpen: boolean
+  open: () => void
+  close: () => void
+  toggle: () => void
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export function selfLoadingCurtain(open: boolean = false){
+  const o: SelfLoadingCurtain = {
+    Element,
+    isOpen: open,
+    open: () => {return;},
+    close: () => {return;},
+    toggle: () => {return;},
+    setOpen: () => {return;}
+  };
+
+  function Element(){
+    const [open, setOpen] = useState(o.isOpen);
+    
+    o.isOpen = open;
+    o.setOpen = setOpen;
+    o.open = () => setOpen(true);
+    o.close = () => setOpen(false);
+    o.toggle = () => setOpen((c) => !c);
+
+    if(!open) return <></>;
+
+    return (
+      <LoadingCurtain/>
+    );
+  }
+
+  return o;
+}
